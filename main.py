e5009d1eeece970da84f5d82cc81f3cffa64aacf
jheave <jimenezjheave123@gmail.com>
2022-06-13 17:32:32 +0800

    add instuctions

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Number guessing game
+
+-- Ask the user to stake a bet
+-- Generate a random number between 1 and 100
+-- Ask the user to guess the number
+-- If the user guesses the number, congratulate them
+-- If the user guesses incorrectly, tell them if they guessed too high or too low
+-- If the user guesses correctly return the amount they bet plus the amount they won
+
+"""
